6174c3a536d5f638170d7d2a9b7c95478acd55f1
Richard Uzsak <Richard.Uzsak@aomvsr.sk>
2024-06-16 11:19:05 +0200

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CheckboxComponent } from './checkbox/checkbox.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CheckboxComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/checkbox/checkbox.component.ts
@@ -0,0 +1,19 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-checkbox',
+  standalone: true,
+  imports: [],
+  templateUrl: './checkbox.component.html',
+  styleUrl: './checkbox.component.css'
+})
+export class CheckboxComponent {
+  @Input() label = "";
+  @Input({transform: checkboxId}) id: string =""; 
+  @Input() name="";
+  @Input() value=""; 
+}
+
+function checkboxId(value: string) {
+  return `${value}-checkbox`;
+}
